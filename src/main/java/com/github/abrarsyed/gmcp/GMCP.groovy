956a801f8b280c6dab5ed07446bdbd6bf0bc955a
AbrarSyed <sacabrarsyed@gmail.com>
2013-05-30 15:10:39 -0500

    fixed up workspace more

---
+++ src/main/java/com/github/abrarsyed/gmcp/GMCP.groovy
@@ -0,0 +1,25 @@
+package com.github.abrarsyed.gmcp;
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+public class GMCP implements Plugin<Project>
+{
+
+	@Override
+	public void apply(Project arg0)
+	{
+		// nothing yet.		
+	}
+
+}
+
+/*
+class GreetingPlugin implements Plugin<Project> {
+	void apply(Project project) {
+		project.task('hello') << {
+			println "Hello from the GreetingPlugin"
+		}
+	}
+}
+*/
