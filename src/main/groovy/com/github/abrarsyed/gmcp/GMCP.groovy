bc252ba0ff76f058e9342ce4f255b38b8c9ee3ea
AbrarSyed <sacabrarsyed@gmail.com>
2013-06-30 21:57:29 -0500

    tried some ant stuff

--- src/main/groovy/com/github/abrarsyed/gmcp/GMCP.groovy
+++ src/main/groovy/com/github/abrarsyed/gmcp/GMCP.groovy
@@ -1,7 +1,8 @@
-package com.github.abrarsyed.gmcp;
+package com.github.abrarsyed.gmcp
 
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.api.tasks.Copy
 
 public class GMCP implements Plugin<Project>
 {
@@ -9,6 +10,12 @@ public class GMCP implements Plugin<Project>
 	public void apply(Project project)
 	{
 		project.extensions.create("minecraft", GMCPExtension)
+
+		// Download stuff
+//		project.task('downloadStuff', type: Copy) {
+//			ant.get(project.minecraft.forgeUrl, project.buildDir, false)
+//		}
+
 	}
 
 }
