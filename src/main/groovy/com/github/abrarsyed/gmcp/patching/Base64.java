4d09ea4815ba1d895acdcd7df842363721a8e0ef
AbrarSyed <sacabrarsyed@gmail.com>
2013-10-10 12:49:23 -0500

    updated git attributes

---
+++ src/main/groovy/com/github/abrarsyed/gmcp/patching/Base64.java
@@ -0,0 +1,12 @@
+package com.github.abrarsyed.gmcp.patching;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: AbrarSyed
+ * Date: 10/10/13
+ * Time: 12:37 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Base64
+{
+}
---
+++ src/main/groovy/com/github/abrarsyed/gmcp/patching/ContextualPatch.java
@@ -0,0 +1,12 @@
+package com.github.abrarsyed.gmcp.patching;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: AbrarSyed
+ * Date: 10/10/13
+ * Time: 12:39 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class ContextualPatch
+{
+}
